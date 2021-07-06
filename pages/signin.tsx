c3357a7773c7e18e46be5b61568ce5a617b7a7c2
Daniel Medina <daniel@medina.com>
2021-07-06 09:02:41 -0500

    Redesigned signin page and it now works

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,30 +1,39 @@
-import { getProviders, signIn } from 'next-auth/client'
+import { getProviders, signIn } from "next-auth/client";
+import Navbar from "../components/Navbar";
 
 export default function SignIn({ providers }: any) {
-  return (
-    <>
-      {Object.values(providers).map((provider: any) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>Sign in with {provider.name}</button>
-        </div>
-      ))}
-    </>
-  )
+    return (
+        <>
+        <Navbar />
+            <main className="flex flex-col p-8">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-4xl md:text-8xl lg:text-9xl tracking-tighter font-bold text-black py-5">
+                        Sign In
+                    </h1>
+                </div>
+                <div className="providers mt-4">
+                    {Object.values(providers).map((provider: any) => (
+                        <div key={provider.name} className="shadow-lg rounded-md hover:bg-blue-300 hover:text-white cursor-pointer mt-1">
+                            <button
+                                onClick={() =>
+                                    signIn(provider.id, { callbackUrl: "/" })
+                                }
+
+                                className="w-full text-left p-5"
+                            >
+                                Sign in with {provider.name}
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </main>
+        </>
+    );
 }
 
-// This is the recommended way for Next.js 9.3 or newer
-export async function getServerSideProps(context: any){
-  const providers = await getProviders()
-  return {
-    props: { providers }
-  }
+export async function getServerSideProps(context: any) {
+    const providers = await getProviders();
+    return {
+        props: { providers },
+    };
 }
-
-/*
-// If older than Next.js 9.3
-SignIn.getInitialProps = async () => {
-  return {
-    providers: await getProviders()
-  }
-}
-*/
