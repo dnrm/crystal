514562351f1284b3cbd372734ecd4d93a204d6f7
Daniel Medina <daniel@medina.com>
2021-07-09 17:47:59 -0500

    Added mechanism to submit post to api

--- pages/create-post.tsx
+++ pages/create-post.tsx
@@ -1,15 +1,69 @@
-import React from "react";
-import { signOut, useSession } from "next-auth/client";
+import React, { useRef } from "react";
+import { signOut, useSession, getSession } from "next-auth/client";
 import Navbar from "../components/Navbar";
 import Head from "next/head";
 
-const CreatePost = () => {
+// * Types
+
+interface Post {
+    title: string;
+    content: string;
+    user: string;
+}
+
+const CreatePost = (props: any) => {
     const [session] = useSession();
+    
+
+    const titleRef: any = useRef();
+    const contentRef: any = useRef();
+
+    const handleSubmit = async (e: any) => {
+        if (session) {
+            e.preventDefault();
+
+            let title = titleRef.current.value;
+            let content = titleRef.current.value;
+
+            try {
+                let response = await createPost({
+                    title,
+                    content,
+                    // @ts-ignore
+                    user: session.user?.email,
+                });
+                console.log(response)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    };
+
+    const createPost = async ({ title, content, user }: Post) => {
+        let api = (process.env.API || '') + '/create-user';
+
+        const response = fetch(api, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: title,
+                lastname: content,
+                user,
+            }),
+        });
+
+        return response
+    };
 
     return (
         <>
             <Head>
                 <title>Create Post | dnrm</title>
+                {props.loggedIn == false ? (
+                    <meta http-equiv="refresh" content="0; url=/" />
+                ) : null}
             </Head>
             <div className="gradient-1 filter blur-3xl bg-teal opacity-20 absolute w-1/2 h-56 rounded-r-full -top-10 -left-8 -z-10"></div>
             <div className="gradient-1 filter blur-3xl bg-neon opacity-20 absolute w-96 h-2/3 top-28 -left-16 -z-10"></div>
@@ -21,82 +75,107 @@ const CreatePost = () => {
                         Create Post
                     </h1>
                 </header>
-                <section
-                    id="form"
-                    className="grid grid-cols-1 md:grid-cols-3 mt-4"
-                >
-                    <div className="upload">
-                        <div className="shadow-md col-span-1 upload-image w-full h-96 bg-white rounded-lg border-dashed border-gray-400 border-2 flex flex-col justify-center items-center hover:bg-blue-100 hover:border-blue-300 hover:text-blue-500">
-                            <img src={"../images/upload.svg"} alt="" />
-                            <h1 className="text-xl font-light text-center leading-6">
-                                Drop image here or{" "}
-                                <label
-                                    htmlFor="upload-image"
-                                    className="hover:underline font-bold"
-                                >
-                                    select from device
-                                </label>
-                            </h1>
-                        </div>
-                        <input
-                            type="file"
-                            name="upload-image"
-                            id="upload-image"
-                            className="hidden"
-                        ></input>
-                    </div>
-                    <div className="pl-8 col-span-2 flex flex-col justify-between items-stretch w-full">
-                        <div className="input-elements">
-                            <div className="group">
-                                <h2 className="text-3xl font-semibold tracking-tighter">
-                                    Title
-                                </h2>
-                                <input
-                                    type="text"
-                                    placeholder="Once upon a time..."
-                                    className="mt-1 p-2 font-lg outline-none w-full border-2 border-gray-300 rounded-lg shadow-md focus:border-blue-400"
-                                />
+                <form onSubmit={handleSubmit}>
+                    <section
+                        id="form"
+                        className="grid grid-cols-1 md:grid-cols-3 mt-4"
+                    >
+                        <div className="upload">
+                            <div className="shadow-md col-span-1 upload-image w-full h-96 bg-white rounded-lg border-dashed border-gray-400 border-2 flex flex-col justify-center items-center hover:bg-blue-100 hover:border-blue-300 hover:text-blue-500">
+                                <img src={"../images/upload.svg"} alt="" />
+                                <h1 className="text-xl font-light text-center leading-6">
+                                    Drop image here or{" "}
+                                    <label
+                                        htmlFor="upload-image"
+                                        className="hover:underline font-bold"
+                                    >
+                                        select from device
+                                    </label>
+                                </h1>
                             </div>
-                            <div className="group mt-4">
-                                <h2 className="text-3xl font-semibold tracking-tighter">
-                                    Description
-                                </h2>
-                                <textarea
-                                    name=""
-                                    id=""
-                                    cols={30}
-                                    rows={5}
-                                    placeholder="The start of a great story..."
-                                    className="mt-1 p-2 font-lg outline-none w-full border-2 border-gray-300 rounded-lg shadow-md resize-none focus:border-blue-400"
-                                ></textarea>
-                            </div>
+                            <input
+                                type="file"
+                                name="upload-image"
+                                id="upload-image"
+                                className="hidden"
+                            ></input>
                         </div>
-                        <div className="buttons flex justify-end">
-                            <button className="px-12 py-3 bg-blue-500 hover:bg-blue-600 text-white shadow-2xl rounded-md outline-none">
-                                Post
-                            </button>
-                            <button className="ml-2 p-3 bg-white hover:bg-gray-200 hover:shadow-none shadow-2xl rounded-md outline-none">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-6 w-6 text-gray-600"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                        <div className="pl-8 col-span-2 flex flex-col justify-between items-stretch w-full">
+                            <div className="input-elements">
+                                <div className="group">
+                                    <h2 className="text-3xl font-semibold tracking-tighter">
+                                        Title
+                                    </h2>
+                                    <input
+                                        type="text"
+                                        ref={titleRef}
+                                        name="title"
+                                        placeholder="Once upon a time..."
+                                        className="mt-1 p-2 font-lg outline-none w-full border-2 border-gray-300 rounded-lg shadow-md focus:border-blue-400"
                                     />
-                                </svg>
-                            </button>
+                                </div>
+                                <div className="group mt-4">
+                                    <h2 className="text-3xl font-semibold tracking-tighter">
+                                        Description
+                                    </h2>
+                                    <textarea
+                                        name="content"
+                                        id=""
+                                        ref={contentRef}
+                                        cols={30}
+                                        rows={5}
+                                        placeholder="The start of a great story..."
+                                        className="mt-1 p-2 font-lg outline-none w-full border-2 border-gray-300 rounded-lg shadow-md resize-none focus:border-blue-400"
+                                    ></textarea>
+                                </div>
+                            </div>
+                            <div className="buttons flex justify-end">
+                                <button
+                                    type="submit"
+                                    className="px-12 py-3 bg-blue-500 hover:bg-blue-600 text-white shadow-2xl rounded-md outline-none"
+                                >
+                                    Post
+                                </button>
+                                <button className="ml-2 p-3 bg-white hover:bg-gray-200 hover:shadow-none shadow-2xl rounded-md outline-none">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6 text-gray-600"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </form>
             </main>
         </>
     );
 };
 
 export default CreatePost;
+
+export async function getServerSideProps(context: any) {
+    const session = await getSession(context);
+    if (!session) {
+        return {
+            props: {
+                loggedIn: false,
+            },
+        };
+    } else {
+        return {
+            props: {
+                loggedIn: true,
+            },
+        };
+    }
+}
