619f77f73722187cfa92e2850dd1f8a6d34ae53c
Daniel Medina <daniel@medina.com>
2021-09-27 12:18:06 -0500

    :bug: lol i was wrong

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Image from 'next/image';
-import src from '../images/boats.jpeg';
+import src from '../images/boats.jpg';
 
 const ServerError = () => {
     return (
